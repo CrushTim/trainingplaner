03d0843e7fbb8bccf73fa8fe8cbc7efacac65715
CrushTim <tgk1702@gmail.com>
2024-12-17 14:28:07 +0100

    list tile for exercise in addplanning exercise dialog

---
+++ lib/frontend/uc06planning/add_planning_exercise_tile.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:trainingplaner/business/businessClasses/training_exercise_bus.dart';
+import 'package:trainingplaner/frontend/uc03TrainingExcercise/reps_weights_row.dart';
+
+class AddPlanningExerciseTile extends StatefulWidget {
+  final TrainingExerciseBus exercise;
+  final Function(TrainingExerciseBus) onUpdate;
+  final Function(TrainingExerciseBus) onDelete;
+
+  const AddPlanningExerciseTile({
+    super.key,
+    required this.exercise,
+    required this.onUpdate,
+    required this.onDelete,
+  });
+
+  @override
+  State<AddPlanningExerciseTile> createState() => _AddPlanningExerciseTileState();
+}
+
+class _AddPlanningExerciseTileState extends State<AddPlanningExerciseTile> {
+  bool isExpanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.black,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: AnimatedSize(
+        duration: const Duration(milliseconds: 500),
+        child: isExpanded
+            ? Column(
+                children: [
+                  ListTile(
+                    title: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          widget.exercise.exerciseName,
+                          style: const TextStyle(
+                            fontSize: 23,
+                            fontWeight: FontWeight.bold,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                        Text(widget.exercise.exerciseFoundationID),
+                      ],
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.arrow_drop_up),
+                      onPressed: () {
+                        widget.onUpdate(widget.exercise);
+                        setState(() {
+                          isExpanded = false;
+                        });
+                      },
+                    ),
+                  ),
+                  for (int i = 0; i < widget.exercise.exerciseReps.length; i++)
+                    RepsWeightsRow(
+                      reps: widget.exercise.exerciseReps[i],
+                      weight: widget.exercise.exerciseWeights[i],
+                      onUpdate: (reps, weight) {
+                        widget.exercise.exerciseReps[i] = reps;
+                        widget.exercise.exerciseWeights[i] = weight;
+                        setState(() {});
+                        
+                      },
+                      onDelete: () {
+                        widget.exercise.exerciseReps.removeAt(i);
+                        widget.exercise.exerciseWeights.removeAt(i);
+                        setState(() {});
+                      },
+                    ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: IconButton(
+                          onPressed: () {
+                            widget.exercise.exerciseReps.add(0);
+                            if (widget.exercise.exerciseWeights.isNotEmpty) {
+                              widget.exercise.exerciseWeights.add(
+                                widget.exercise.exerciseWeights[
+                                  widget.exercise.exerciseWeights.length - 1],
+                              );
+                            } else {
+                              widget.exercise.exerciseWeights.add(0);
+                            }
+                            setState(() {});
+                          },
+                          icon: const Icon(Icons.add),
+                        ),
+                      ),
+                      Expanded(
+                        child: IconButton(
+                          icon: const Icon(Icons.delete),
+                          onPressed: () => widget.onDelete(widget.exercise),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            : ListTile(
+                title: Text(widget.exercise.exerciseName),
+                subtitle: Text(
+                  "${widget.exercise.exerciseReps.join('x')} reps @ ${widget.exercise.exerciseWeights.join(', ')}kg",
+                ),
+                trailing: IconButton(
+                  icon: const Icon(Icons.arrow_drop_down),
+                  onPressed: () {
+                    setState(() {
+                      isExpanded = true;
+                    });
+                  },
+                ),
+              ),
+      ),
+    );
+  }
+}
--- lib/frontend/uc06planning/add_planning_session_dialog.dart
+++ lib/frontend/uc06planning/add_planning_session_dialog.dart
@@ -4,6 +4,7 @@ import 'package:trainingplaner/business/businessClasses/training_exercise_bus.da
 import 'package:trainingplaner/frontend/costum_widgets/date_picker_sheer.dart';
 import 'package:trainingplaner/frontend/uc03TrainingExercise/add_exercise_edit_fields.dart';
 import 'package:trainingplaner/frontend/uc06planning/planning_provider.dart';
+import 'package:trainingplaner/frontend/uc06planning/add_planning_exercise_tile.dart';
 
 class AddPlanningSessionDialog extends StatefulWidget {
   final DateTime initialDate;
@@ -33,83 +34,99 @@ class _AddPlanningSessionDialogState extends State<AddPlanningSessionDialog> {
     final provider = Provider.of<PlanningProvider>(context);
 
     return Dialog(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            TextField(
-              controller: provider.sessionNameController,
-              decoration: const InputDecoration(labelText: 'Session Name'),
-              onChanged: (value) => provider.handleSessionFieldChangeForPlanned('name', value),
-            ),
-            TextField(
-              controller: provider.sessionDescriptionController,
-              decoration: const InputDecoration(labelText: 'Description'),
-              onChanged: (value) => provider.handleSessionFieldChangeForPlanned('description', value),
-            ),
-            TextField(
-              controller: provider.sessionEmphasisController,
-              decoration: const InputDecoration(labelText: 'Emphasis (comma separated)'),
-              onChanged: (value) => provider.handleSessionFieldChangeForPlanned('emphasis', value),
-            ),
-            TextField(
-              controller: provider.sessionLengthController,
-              decoration: const InputDecoration(labelText: 'Length (minutes)'),
-              keyboardType: TextInputType.number,
-              onChanged: (value) => provider.handleSessionFieldChangeForPlanned('length', value),
-            ),
-            DatePickerSheer(
-              initialDateTime: provider.selectedSessionDate,
-              onDateTimeChanged: provider.updateSessionDate,
-              dateController: TextEditingController(
-                text: provider.selectedSessionDate.toString()
+      child: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: provider.sessionNameController,
+                decoration: const InputDecoration(labelText: 'Session Name'),
+                onChanged: (value) => provider.handleSessionFieldChangeForPlanned('name', value),
               ),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(onPressed: () {
-              showDialog(context: context, builder: (context) => ChangeNotifierProvider.value(value: provider, child: const AddExerciseEditFields(addPlanned: true)),).then((value) {
-                
-                setState(() {
+              TextField(
+                controller: provider.sessionDescriptionController,
+                decoration: const InputDecoration(labelText: 'Description'),
+                onChanged: (value) => provider.handleSessionFieldChangeForPlanned('description', value),
+              ),
+              TextField(
+                controller: provider.sessionEmphasisController,
+                decoration: const InputDecoration(labelText: 'Emphasis (comma separated)'),
+                onChanged: (value) => provider.handleSessionFieldChangeForPlanned('emphasis', value),
+              ),
+              TextField(
+                controller: provider.sessionLengthController,
+                decoration: const InputDecoration(labelText: 'Length (minutes)'),
+                keyboardType: TextInputType.number,
+                onChanged: (value) => provider.handleSessionFieldChangeForPlanned('length', value),
+              ),
+              DatePickerSheer(
+                initialDateTime: provider.selectedSessionDate,
+                onDateTimeChanged: provider.updateSessionDate,
+                dateController: TextEditingController(
+                  text: provider.selectedSessionDate.toString()
+                ),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(onPressed: () {
+                showDialog(context: context, builder: (context) => ChangeNotifierProvider.value(value: provider, child: const AddExerciseEditFields(addPlanned: true)),).then((value) {
+                  
+                  setState(() {
+                  });
+                  provider.exerciseProvider.resetBusinessClassForAdd();
                 });
-                provider.exerciseProvider.resetBusinessClassForAdd();
-              });
-            }, child: const Text("Add Exercise")),
-            provider.getSelectedBusinessClass != null ?
-            Column(
-              children: List.generate(
-                provider.getSelectedBusinessClass!.trainingSessionExercises.length,
-                (index) {
-                  //TODO: add Exercise in AddPlanningListTile 
-                  final name = provider.getSelectedBusinessClass!.trainingSessionExercises[index].getName();
-                  return Text(name);
-                },
-              ),
-            )
-            : Container(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                TextButton(
-                  onPressed: () => Navigator.pop(context),
-                  child: const Text('Cancel'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    if (provider.getSelectedBusinessClass != null) {
-                      provider.getSelectedBusinessClass!.trainingCycleId = widget.cycleId;
-                    } else {
-                      provider.businessClassForAdd.trainingCycleId = widget.cycleId;
-                    }
-                    provider.saveSession(context);
+              }, child: const Text("Add Exercise")),
+              provider.getSelectedBusinessClass != null ?
+              Column(
+                children: List.generate(
+                  provider.getSelectedBusinessClass!.trainingSessionExercises.length,
+                  (index) {
+                    final exercise = provider.getSelectedBusinessClass!.trainingSessionExercises[index];
+                    return AddPlanningExerciseTile(
+                      exercise: exercise,
+                      onUpdate: (updatedExercise) {
+                        ScaffoldMessengerState scaffoldMessenger = ScaffoldMessenger.of(context);
+                        provider.exerciseProvider.updateBusinessClass(updatedExercise, scaffoldMessenger);
+                        if(provider.getSelectedBusinessClass != null){
+                          provider.updateSelectedBusinessClass(scaffoldMessenger);
+                        } 
+                      },
+                      onDelete: (exercise) {
+                        setState(() {
+                          provider.getSelectedBusinessClass!.trainingSessionExercises.removeAt(index);
+                        });
+                      },
+                    );
                   },
-                  child: Text(provider.getSelectedBusinessClass != null ? 'Update' : 'Save'),
                 ),
-              ],
-            ),
-          ],
+              )
+              : Container(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text('Cancel'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (provider.getSelectedBusinessClass != null) {
+                        provider.getSelectedBusinessClass!.trainingCycleId = widget.cycleId;
+                      } else {
+                        provider.businessClassForAdd.trainingCycleId = widget.cycleId;
+                      }
+                      provider.saveSession(context);
+                    },
+                    child: Text(provider.getSelectedBusinessClass != null ? 'Update' : 'Save'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-      ),
+      ]),
     );
   }
 } 
